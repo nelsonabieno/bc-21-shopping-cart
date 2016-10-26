1a5304cfd06c1f0a7f2ec7f8ba235fee9f2ad281
nelsonabieno <nelsonabieno@gmail.com>
2016-10-26 11:03:45 -0700

    Display list of featured items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,48 @@
 //
 
 $(document).ready(function (e) {
-	//alert('yoo');
-	$('.img_draggable').draggable();
+
+	$('[data-toggle="tooltip"]').tooltip();
+
+	$('#item1').draggable();
+	$('#item2').draggable();
+	$('#item3').draggable();
+	$('#item4').draggable();
+	$('#item5').draggable();
+	$('#item6').draggable();
+
+
+	$("#cartDiv").droppable({
+		activeClass:"highlight",
+		hoverClass:"hoverDroppable",
+		drop: function(event,ui){
+			//ui.helper.hide("fade");
+			var itemAlt=$(ui.draggable).attr("alt");
+			var id=$(ui.draggable).attr("id");
+			alert("Item "+itemAlt+" added to Cart");
+		},
+		
+		disabled:false,
+		activate:function(event,ui){
+			$("#cartMsg").remove();
+			$(this).append("<span id='cartMsg'>Drop Item Here </span>");
+		},
+
+		deactivate:function(event,ui){
+			$("#cartMsg").remove();
+			$(this).append("<span id='cartMsg'>You Know You Want It</span>");
+		},
+
+		over:function(event,ui){
+			$("#cartMsg").remove();
+			$(this).append("<span id='cartMsg'>Drop It</span>");
+		},
+
+		out:function(event,ui){
+			$("#cartMsg").remove();
+			$(this).append("<span id='cartMsg'>NOOOOOO!</span>");
+		}
+	});
 });
+
+
